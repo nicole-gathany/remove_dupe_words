a4f711d06db8bda46c3cae2f7126bb701c683d4a
Nicole Gathany <nicole.gathany@gmail.com>
2021-08-12 01:40:14 +0000

    added instructions

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+// Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+
+// Example:
+
+// Input:
+
+// 'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+// Output:
+
+// 'alpha beta gamma delta'
+
 function removeDuplicateWords (s) {
 let arr = [...new Set(s.split(' '))]
 return arr.join(' ')
